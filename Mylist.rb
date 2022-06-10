826881353c7be3998190e4f91ca71bb221c4e66c
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-10 13:50:19 +0300

    Update included the Mylist class to the project

---
+++ Mylist.rb
@@ -0,0 +1,19 @@
+class Mylist
+  def initialize(*list)
+    @list = list
+  end  def each
+    (0...@list.length).each do |list|
+      yield @list[list]
+    end
+  end
+endlist = Mylist.new(1, 2, 3, 4)# Test #all?
+puts list.all? {|e| e < 5}
+# => true
+puts list.all? {|e| e > 5}
+# => false# # Test #any?
+puts list.any? {|e| e == 2}
+# => true
+puts list.any? {|e| e == 5}
+# => false# # Test #filter
+puts list.filter {|e| e.even?}
+# => [2, 4]
